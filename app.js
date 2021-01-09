38023f863c19c4af8406baaf4f1d5fcded3dcdaa
tancao-dev <tancaominh98@gmail.com>
2021-01-09 13:41:13 +0700

    fix cookie

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // express session
-var oneThousandDay = 24 * 60 * 60 * 1000 * 20;
+var oneThousandDay = 24 * 60 * 60 * 1000;
 app.use(session(
 {
 	secret: 'secret',
